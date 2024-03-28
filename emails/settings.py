d803267b7272a4e22430afdf1640a8ca1a4086c4
mnibor <marcelorobin@gmail.com>
2024-03-28 11:27:49 -0300

    cambios

--- emails/settings.py
+++ emails/settings.py
@@ -128,5 +128,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'codigoparaprincipiantes@gmail.com'
-EMAIL_HOST_PASSWORD = 'xipjxwicvhcylojl'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
